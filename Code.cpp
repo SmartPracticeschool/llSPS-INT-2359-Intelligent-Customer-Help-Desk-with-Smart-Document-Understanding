81d130c220a6be1f4c948d13b398fe07cc63c622
Eshwar17 <65604208+Eshwar17@users.noreply.github.com>
2020-08-09 00:06:04 +0530

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<algorithm>
+#include<array>
+#include<map>
+#include<string>
+#include<vector>
+#include<math.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--)
+ {
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++) 
+  {
+   cin>>arr[i];
+  } 
+  int gift=1;
+  int min_gift=1;
+   for(int i=0;i<n-1;i++)
+   {
+    if(arr[i+1]>arr[i])
+    {
+     gift++;
+     min_gift+=gift;
+    }
+    else
+    {
+     if(gift>1)
+     {
+      gift--;
+      min_gift+=gift;
+     }
+     else
+     {
+      min_gift+=gift;
+     }
+    }
+//    cout<<arr[i+1]<<"gift "<<gift<<"\n";
+   }
+   cout<<min_gift<<"\n";
+ }  
+}
